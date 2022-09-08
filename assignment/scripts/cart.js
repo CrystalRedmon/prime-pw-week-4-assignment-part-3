b742fd4a6e1a4fd4652f10b500b24c856dea5d68
Crystal Redmon <cato2848@gmail.com>
2022-09-08 09:50:57 -0500

    created listItems and empty functions

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -3,8 +3,29 @@ console.log('***** Cart Functions *****');
 // We want to see how you are testing your code!!!
 
 let basket=[];
-
+///
 function addItem(item){
     basket.push(item);
     return true;
 }
+
+addItem('bananas');
+console.log(basket);
+
+////
+function listItems(){
+    for(let i=0; i<basket.length; i++){
+        console.log(basket[i]);
+    }
+}
+addItem('milk')
+listItems();
+////
+
+function empty(){
+    basket=[];
+}
+
+empty();
+console.log(basket)
+////
